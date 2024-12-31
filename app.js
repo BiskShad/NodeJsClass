03d6f61961a89406817c6b2be57741a5258e300e
Bismark Guevara <b.gue.mquez@gmail.com>
2024-12-31 13:20:22 -0600

    Add Event drive

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const http = require('http');
+const routes = require('./route');
 
 
-const server = http.createServer((req, res ) => {
-  console.log(req.url, req.method, req.headers);
-  res.setHeader('Content-type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My first page</title></head>');
-  res.write('<body><h1>Hello from my node js server</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+
+const server = http.createServer(routes); 
+  
+ 
+
 
 server.listen(3000);
