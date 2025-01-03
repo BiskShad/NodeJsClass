c189930528ee512e247b870fcf9f7f8a01f01c5f
Bismark Guevara <b.gue.mquez@gmail.com>
2025-01-03 14:02:32 -0600

    Changing pug template and handlebars for EJS template

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+//const expressHbs = require('express-handlebars'); CUANDO QUIERAR HANDLEBARS COMO TEMPLATE
 
 const app = express();
 
 //installing express pug dinamic template
-app.set('view engine' , 'pug');
+//app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout:'main-layout', extname:'hbs' }));
+app.set('view engine' , 'ejs');
 app.set('views', 'views');
 
 //Routes Link
@@ -21,8 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
+//Acess response to 404.html
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+   res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 
