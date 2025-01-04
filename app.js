7a1bd60d2f0ac9f3fb039c0335a8720d910b3ada
Bismark Guevara <b.gue.mquez@gmail.com>
2025-01-03 20:12:26 -0600

    Adding controllers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+
 //const expressHbs = require('express-handlebars'); CUANDO QUIERAR HANDLEBARS COMO TEMPLATE
 
+const errorController = require('./controllers/error')
 const app = express();
 
 //installing express pug dinamic template
@@ -11,7 +13,7 @@ app.set('view engine' , 'ejs');
 app.set('views', 'views');
 
 //Routes Link
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,13 +22,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Acces to admin Routes
-app.use('/admin',adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//Acess response to 404.html
-app.use((req, res, next) => {
-   res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
-
+//Acess response to 404 ejs
+app.use(errorController.get404);
 
 app.listen(3000);  
