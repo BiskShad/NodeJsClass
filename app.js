4336651febc67b218ea54a0cc8265901191758df
Bismark Guevara <b.gue.mquez@gmail.com>
2025-02-06 21:59:02 -0600

    Database seuqelize no connected

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error')
 
 //exportando la base de datos
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -34,4 +34,15 @@ app.use(shopRoutes);
 //Acess response to 404 ejs
 app.use(errorController.get404);
 
-app.listen(3000);  
+sequelize
+    .sync()
+    .then(result => {
+    console.log(result);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+
+});
+
+  
